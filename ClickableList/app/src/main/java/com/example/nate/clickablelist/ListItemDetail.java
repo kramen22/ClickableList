8af4af0b0954b1dd1ff0999bbb0f809bab11cde2
Nate Kramer <kramernate0@gmail.com>
2017-03-08 17:29:35 -0600

    Second Commit

--- ClickableList/app/src/main/java/com/example/nate/clickablelist/ListItemDetail.java
+++ ClickableList/app/src/main/java/com/example/nate/clickablelist/ListItemDetail.java
@@ -3,12 +3,16 @@ package com.example.nate.clickablelist;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 /**
  * Created by nate on 3/7/17.
  */
-public class ListItemDetail extends Activity {
+public class ListItemDetail extends Activity implements AdapterView.OnItemClickListener{
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,7 +28,13 @@ public class ListItemDetail extends Activity {
         TextView myTextView = (TextView) findViewById(R.id.my_textview);
         myTextView.setText(myKeys[position]);
 
+        ListView listview = (ListView) findViewById(R.id.listView2);
+        listview.setOnItemClickListener(this);
 
     }
 
+    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+        Log.i("HelloListView", "You clicked Item: " + id + " at position:" + position);
+        // Then you start a new Activity via Intent
+    }
 }
